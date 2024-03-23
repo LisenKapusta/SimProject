abeb9aeec5602d9bf6fcde4ddffb1c5addad3683
LisenKapusta <elizavetaholodkova@gmail.com>
2024-03-23 14:30:21 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+bot = Bot(token="...")
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
