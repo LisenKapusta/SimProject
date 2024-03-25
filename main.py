2e2c02f9b36d30949f4871d5c8103fc8426ededd
LisenKapusta <elizavetaholodkova@gmail.com>
2024-03-25 12:30:11 +0300

    init commit

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from sim_project.tts import SimlishTTS
+from sim_project.gpt_translate import SimlishTranslate
 
 
 bot = Bot(token=os.environ["BOT_API"])
 dp = Dispatcher()
 
+translate_answer = SimlishTranslate()
 @dp.message()
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer(translate_answer(message))
 async def main():
     await dp.start_polling(bot)
 
